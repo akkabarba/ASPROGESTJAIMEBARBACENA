5a4266035348db60ebb0b9392027cca78f3867c8
akkabarba <xjbarace307@ieshnosmachado.org>
2025-05-28 17:02:37 +0200

    cambios rutas y tal

--- incidencias/urls.py
+++ incidencias/urls.py
@@ -8,12 +8,11 @@ router = DefaultRouter()
 router.register(r'incidencias', IncidenciaViewSet, basename='incidencias')
 
 urlpatterns = [
-    path('api/token/', views.token_obtain_pair, name='token_obtain_pair'),
-    path('api/token/refresh/', views.token_refresh, name='token_refresh'),
-    path('api/whoami/', views.whoami, name='whoami'),
-    path('api/crear_usuario/', views.crear_usuario_api, name='crear_usuario_api'),
-    path('api/usuarios/', views.listar_usuarios_api, name='listar_usuarios_api'),
-    path('api/incidencias_nuevas/', views.contar_incidencias_nuevas, name='contar_incidencias_nuevas'),
-    path('api/', include(router.urls)),
+    path('token/', views.token_obtain_pair, name='token_obtain_pair'),
+    path('token/refresh/', views.token_refresh, name='token_refresh'),
+    path('whoami/', views.whoami, name='whoami'),
+    path('crear_usuario/', views.crear_usuario_api, name='crear_usuario_api'),
+    path('usuarios/', views.listar_usuarios_api, name='listar_usuarios_api'),
+    path('incidencias_nuevas/', views.contar_incidencias_nuevas, name='contar_incidencias_nuevas'),
+    path('', include(router.urls)),  
 ]
-
