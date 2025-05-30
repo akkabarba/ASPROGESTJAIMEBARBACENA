87460166e5a5b843aa956aadc4807b49835f8be7
akkabarba <xjbarace307@ieshnosmachado.org>
2025-05-30 12:43:29 +0200

    cambios para probar

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,2 @@
-const API_BASE = "https://asprogestjaimebarbacena.onrender.com/api" || 'http://localhost:8000/api';
+const API_BASE = process.env.REACT_APP_API_URL || 'https://asprogestjaimebarbacena.onrender.com/api';
 export default API_BASE;
