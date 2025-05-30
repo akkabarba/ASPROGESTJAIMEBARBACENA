a54121b852c9a148f3b9c17e03ccfc4318ba3765
akkabarba <xjbarace307@ieshnosmachado.org>
2025-05-30 10:52:26 +0200

    cambio

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,2 @@
-const API_BASE = "https://asprogest.onrender.com/api" || 'http://localhost:8000/api';
+const API_BASE = "https://asprogestjaimebarbacena.onrender.com/api" || 'http://localhost:8000/api';
 export default API_BASE;
