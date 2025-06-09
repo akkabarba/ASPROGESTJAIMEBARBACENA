bf72485ce6687bead482267abc577f4bda26442f
akkabarba <xjbarace307@ieshnosmachado.org>
2025-06-09 13:14:37 +0200

    cambios para los modelos y migraciones

--- incidencias/admin.py
+++ incidencias/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Incidencia, Equipo
+from .models import Incidencia, Ordenador, Telefono, EquipoRed, Impresora
 
 
 admin.site.register(Incidencia)
-admin.site.register(Equipo)
+admin.site.register(Ordenador)
+admin.site.register(Telefono)
+admin.site.register(EquipoRed)                
+admin.site.register(Impresora)
